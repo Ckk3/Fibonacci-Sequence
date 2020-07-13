db70eb7f09c60fc85fb1b1895027b5e033b17594
Ckk3 <gugamotamiranda@hotmail.com>
2020-07-13 09:48:41 -0300

    translatade the variables

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -6,35 +6,33 @@ namespace Fibonacci {
         static void Main(string[] args) {
             //Declarando Variáveis
             List<int>  fibonacci;
-            int anterior, anterior2, primeiro, segundo, quant;
+            int previous, anterior2, first, second, amount;
 
             //Iniciando Variáveis
-            fibonacci = new List<int> { };
-            anterior = 0;
-            anterior = 0;
-            primeiro = 0;
-            segundo = 0;
-            quant = 0;
+            fibonacci = new List<int> { };   
+            first = 0;
+            second = 0;
+            amount = 0;
 
-            Console.Write("Digite o primeiro número: ");
-            primeiro = int.Parse(Console.ReadLine().ToString());
-            fibonacci.Add(primeiro);
+            Console.Write("Enter the first number: ");
+            first = int.Parse(Console.ReadLine().ToString());
+            fibonacci.Add(first);
 
-            Console.Write("Digite o segundo número: ");
-            segundo = int.Parse(Console.ReadLine().ToString());
-            fibonacci.Add(segundo);
+            Console.Write("The second number: ");
+            second = int.Parse(Console.ReadLine().ToString());
+            fibonacci.Add(second);
 
-            Console.Write("Digite quantos números você quer na sequência: ");
-            quant = int.Parse(Console.ReadLine().ToString());
+            Console.Write("How many numbers you want add to the sequence: ");
+            amount = int.Parse(Console.ReadLine().ToString());
 
 
-            for(int num = 0;num != quant;num++) {
+            for(int num = 0;num != amount;num++) {
                 fibonacci.Add(fibonacci[(fibonacci.Count - 1)] + fibonacci[(fibonacci.Count - 2)]);
             }
 
-            Console.WriteLine($"Os próximos números na sequência são:");
-            foreach(var numero in fibonacci) {
-                Console.WriteLine(numero);
+            Console.WriteLine($"The next numbers in the sequence are:");
+            foreach(var number in fibonacci) {
+                Console.WriteLine(number);
             }
 
             Console.ReadKey();
